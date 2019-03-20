2086646ccb3faa6dae0e06aa0c209aa29b89492c
jonay <jonaydam1@gmail.com>
2019-03-20 19:44:49 +0100

    green: concatenate string

--- koans/main.js
+++ koans/main.js
@@ -4,4 +4,20 @@ describe("koans",function(){
     it("test green", function(){
         assert.equal(true, true);
     });
+
+    describe("Basic Programming", function(){
+
+        describe("1. What is it javascript?", function(){
+            it("concatenate string",function(){
+                let text1 = "Hi,";
+                let text2 = "David";
+
+                let result = text1text2;
+
+                assert.equal(result, "Hi,David");
+            });
+        });
+
+
+    });
 });
