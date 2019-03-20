07dbc84280e6d752404b1787c90bd7221c93aba7
jonay <jonaydam1@gmail.com>
2019-03-20 20:22:04 +0100

    add increment and decrement

--- koans/main.js
+++ koans/main.js
@@ -25,18 +25,36 @@ describe("koans",function(){
 
                 assert.equal(result, 3);
             });
+        });
+    });
+    describe("2. Variables", function(){
+        it("different between let and const",function(){
+            let text1 = "Hi";
+            text1 = "Bye";
+            const text2 = "Bye";
+            text2 = "Hi";
 
-            it("different between let and const",function(){
-                let text1 = "Hi";
-                text1 = "Bye";
-                const text2 = "Bye";
-                text2 = "Hi";
-
-                assert.equal(text1, "Hi");
-                assert.equal(text2, "Bye");
-            });
+            assert.equal(text1, "Hi");
+            assert.equal(text2, "Bye");
         });
 
+        it("Calculate the operation without parenthesis", function(){
+            let result = 2+5+4-1*(2+3);
+
+            assert.equal(result, 12);
+        });
+
+        it("increment and decrement", function(){
+            let number1 = 1;
+            let number2 = 2;
+
+            number1++;
+            number2--;
+
+            assert.equal(number1, 1);
+            assert.equal(number2, 2);
+        });
 
     });
+
 });
