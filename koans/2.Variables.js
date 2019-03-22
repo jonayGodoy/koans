c9a11dfaf4924df652f3e49e14543a7e171a9571
jonay <jonaydam1@gmail.com>
2019-03-22 07:32:05 +0100

    fix koans

--- koans/2.Variables.js
+++ koans/2.Variables.js
@@ -1,26 +1,5 @@
 let assert = chai.assert;
 
-
-describe("1. What is it javascript?", function(){
-    it("concatenate string",function(){
-        let text1 = "Hi,";
-        let text2 = "David";
-
-        let result = text1text2;
-
-        assert.equal(result, "Hi,David");
-    });
-
-    it("add number",function(){
-        let number1 = 1;
-        let number2 = 2;
-
-        let result = number1-number2;
-
-        assert.equal(result, 3);
-    });
-});
-});
 describe("2. Variables", function() {
     it("different between let and const", function () {
         let text1 = "Hi";
