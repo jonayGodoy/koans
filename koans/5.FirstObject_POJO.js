4385f94c2d3b193f2704e1560f714c20db7d7ca5
jonay <jonaydam1@gmail.com>
2019-03-30 12:20:39 +0100

    create part about POJO

---
+++ koans/5.FirstObject_POJO.js
@@ -0,0 +1,30 @@
+let assert = chai.assert;
+
+describe("5 The first object, POJO(Plain Old Object Javascript)", function(){
+
+    it("properties", function(){
+        let user = {
+            name: "John",
+            age: 30
+        };
+
+        let message = "My name is "+user.age+" and i am "+user.name+" years old";
+
+        assert.equal(message, "My name is John and i am 30 years old");
+    });
+
+
+    it("increment + age", function(){
+        //In this exercise you can change the assert
+
+        let user = {
+            name: "John",
+            age: 30
+        };
+
+        user.age += 1;
+
+        assert.equal(user.age, 30);
+    });
+
+});
